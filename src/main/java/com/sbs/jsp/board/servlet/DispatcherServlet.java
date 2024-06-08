befd62ec3c465c9058e55f78e44748ec451033f5
SangWon7242 <gotkdqja@gmail.com>
2024-06-08 11:09:20 +0900

    기본 라우팅 기능 구현

--- src/main/java/com/sbs/jsp/board/servlet/DispatcherServlet.java
+++ src/main/java/com/sbs/jsp/board/servlet/DispatcherServlet.java
@@ -1,5 +1,8 @@
 package com.sbs.jsp.board.servlet;
 
+import com.sbs.jsp.board.Rq;
+import com.sbs.jsp.board.article.ArticleController;
+import com.sbs.jsp.board.member.MemberController;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,6 +15,23 @@ import java.io.IOException;
 public class DispatcherServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    resp.getWriter().append("<h1>Hello</h1>");
+    Rq rq = new Rq(req, resp);
+
+    MemberController memberController = new MemberController();
+    ArticleController articleController = new ArticleController();
+
+    String url = req.getRequestURI();
+
+    switch (url) {
+      case "/usr/article/list" -> {
+        articleController.showList(rq);
+      }
+      case "/usr/member/login" -> {
+        memberController.showLogin(rq);
+      }
+    }
+
+
+    System.out.println(url);
   }
 }
